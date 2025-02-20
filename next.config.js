0db8c9ed7ef73c8d7e34cadf9cb424e25cb0d25c
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-02-20 12:18:31 -0500

    fix prod photos

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), '@aws-sdk/client-s3'];
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
@@ -9,13 +15,7 @@ const nextConfig = {
         pathname: '/**',
       }
     ],
-  },
-  env: {
-    NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID,
-    NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY: process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY,
-    NEXT_PUBLIC_AWS_S3_REGION: process.env.NEXT_PUBLIC_AWS_S3_REGION,
-    NEXT_PUBLIC_AWS_S3_BUCKET_NAME: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
-  },
+  }
 };
 
 module.exports = nextConfig; 
