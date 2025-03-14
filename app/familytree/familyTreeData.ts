5cd7c787d0e90e555ffb640e0a135bbcbb3f0b81
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-03-14 12:20:28 -0400

    add back familyTreeData object

---
+++ app/familytree/familyTreeData.ts
@@ -0,0 +1,95 @@
+export const familyTreeData = {
+  name: "Cynthia",
+  children: [
+    {
+      name: "Damaris",
+      children: [
+        { 
+          name: "Nicoletta",
+          children: [
+            { name: "Victor" },
+            { name: "Brooklyn" },
+            { name: "Miles" },
+            { name: "Fabrice" }
+          ]
+        },
+        { name: "Christina" }
+      ],
+    },
+    {
+      name: "Sharon",
+      children: [
+        { 
+          name: "Victor",
+          spouse: "Stefania",
+          children: [{ name: "Alani" }, { name: "Ianna" }]
+        }, 
+        { name: "Jonathan" }, 
+        { name: "Samantha" }
+      ],
+    },
+    {
+      name: "Brenda",
+      children: [
+        { 
+          name: "FaTina", 
+          children: [
+            { name: "Tristan" },
+            { name: "Brenna" },
+            { name: "Ethan" },
+            { name: "Aiden" }
+          ]
+        },
+        { name: "Clifton" }
+      ],
+    },
+    {
+      name: "Vanessa",
+      children: [
+        { 
+          name: "Raleigh",
+          spouse: "Ashley",
+          children: [
+            { name: "Bailey" },
+            { name: "Isaiah" }
+          ],
+          previousSpouses: [
+            {
+              name: "Sandra",
+              children: [
+                { name: "Genesis" },
+                { name: "Isaiah" },
+                { name: "Abigail" }
+              ]
+            }
+          ]
+        }, 
+        { 
+          name: "Lisa", 
+          children: [
+            { name: "Taleyah" },
+            { name: "Tiara" },
+            { name: "Treyvon" }
+          ]
+        }, 
+        { name: "Maurice" }
+      ],
+    },
+    {
+      name: "Frances",
+      children: [{ name: "Ronald" }, { name: "Anthony" }, { name: "Aristalia" }, { name: "John Paul" }],
+    },
+    {
+      name: "Angela",
+      children: [{ name: "Jessica" }, { name: "Justin" }, { name: "Chevone" }, { name: "Aubrey" }, { name: "Kintara" }, { name: "TJ" }],
+    },
+    {
+      name: "Mario",
+      children: [{ name: "Jahson" }, { name: "Javin" }, { name: "Jamari" }],
+    },    
+    {
+      name: "Sarih",
+      children: [{ name: "Chyna" }, { name: "Mya" }, { name: "Noah" }],
+    },
+  ],
+};
