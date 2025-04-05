a8b6b983ff0c24dfeba4d6a12874d2cdd42f5d61
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-04-05 17:05:23 -0400

    add seeded data birthdays

---
+++ app/calendar/calendarData.ts
@@ -0,0 +1,95 @@
+import { CalendarEvent } from '@/context/CalendarContext';
+
+export const DEFAULT_EVENTS: CalendarEvent[] = [
+  {
+    id: '1',
+    title: "Jessica's Birthday",
+    start: "2025-01-19T19:00:00.000Z",
+    allDay: true,
+    backgroundColor: "#ADD8E6",
+  },
+  {
+    id: '2',
+    title: "Nicoletta's Birthday",
+    start: "2025-03-17T00:00:00.000Z",
+    allDay: true,
+    backgroundColor: "#ADD8E6",
+  },
+  {
+    id: '3',
+    title: "Victor's Birthday",
+    start: "2025-03-17T00:00:00.000Z",
+    allDay: true,
+    backgroundColor: "#ADD8E6",
+  },
+  {
+    id: '4',
+    title: "Javin's Birthday",
+    start: "2025-03-21T00:00:00.000Z", // Brother's birthday
+    allDay: true,
+    backgroundColor: "#ADD8E6",
+  },
+  {
+    id: '5',
+    title: "Samantha's Birthday",
+    start: "2025-03-25T19:00:00.000Z",
+    allDay: true,
+    backgroundColor: "#ADD8E6",
+  },
+  {
+    id: '6',
+    title: "Maurices' Birthday",
+    start: "2025-04-16T00:00:00.000Z", // Brother's birthday
+    allDay: true,
+    backgroundColor: "#ADD8E6", // Optional: Set a background color
+  },
+  {
+    id: '7',
+    title: "Brenna's Birthday",
+    start: "2025-04-26T00:00:00.000Z", // Brother's birthday
+    allDay: true,
+    backgroundColor: "#ADD8E6", // Optional: Set a background color
+  },
+  {
+    id: '8',
+    title: "Sharon's Birthday",
+    start: "2025-06-10T00:00:00.000Z", // Brother's birthday
+    allDay: true,
+    backgroundColor: "#ADD8E6", // Optional: Set a background color
+  },
+  {
+    id: '9',
+    title: "Chevone's Birthday",
+    start: "2025-07-15T00:00:00.000Z", // Brother's birthday
+    allDay: true,
+    backgroundColor: "#ADD8E6", // Optional: Set a background color
+  },
+  {
+    id: '10',
+    title: "Jonathan's Birthday",
+    start: "2025-07-19T00:00:00.000Z", // Brother's birthday
+    allDay: true,
+    backgroundColor: "#ADD8E6", // Optional: Set a background color
+  },
+  {
+    id: '11',
+    title: "Ianna's Birthday",
+    start: "2025-08-04T00:00:00.000Z", // Brother's birthday
+    allDay: true,
+    backgroundColor: "#ADD8E6", // Optional: Set a background color
+  },
+  {
+    id: '12',
+    title: "Christina's Birthday",
+    start: "2025-08-19T00:00:00.000Z", // Brother's birthday
+    allDay: true,
+    backgroundColor: "#ADD8E6", // Optional: Set a background color
+  },
+  {
+    id: '13',
+    title: "Alani's Birthday",
+    start: "2025-12-30T00:00:00.000Z", // Brother's birthday
+    allDay: true,
+    backgroundColor: "#ADD8E6", // Optional: Set a background color
+  }
+]; 
