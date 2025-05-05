3f43f459f84aded355b694865c5132b6d5eda387
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-05-05 17:00:39 -0400

    navbar items ui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -59,7 +59,7 @@ export default function Navbar({ signOut, username, userFirstName, userLastName,
                     key={item.name}
                     href={item.href}
                     aria-current={item.current ? 'page' : undefined}
-                    className={`${item.current ? 'text-gray-800' : 'text-gray-300 hover:bg-gray-700 hover:text-gray-800'} btn rounded-md px-3 py-2 text-sm font-medium bg-transparent border-0 hover:bg-transparent`}
+                    className={`${item.current ? 'text-gray-800' : 'text-gray-500 hover:bg-gray-700 hover:text-gray-800'} btn rounded-md px-3 py-2 text-sm font-medium bg-transparent border-0 hover:bg-transparent`}
                   >
                     {item.name}
                   </a>
