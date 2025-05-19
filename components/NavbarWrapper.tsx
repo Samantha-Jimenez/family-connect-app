c2c9e31ec544b138607533b943bec34a9d0433ff
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-05-19 19:43:11 -0400

    finalize auth page

--- components/NavbarWrapper.tsx
+++ components/NavbarWrapper.tsx
@@ -32,17 +32,17 @@ export default function NavbarWrapper({ children }: { children: React.ReactNode 
   if (!user) {
     return (
       <div className="flex justify-center items-stretch min-h-screen">
-        <div className="grid lg:grid-cols-2 items-center gap-8 max-w-6xl max-lg:max-w-xl w-full content-evenly">
-          <div className="px-6 pt-8 lg:pt-0">
+        <div className="grid lg:grid-cols-2 items-center max-w-6xl max-lg:max-w-[30rem] w-full justify-center">
+          <div className="px-6 py-8 lg:pt-0">
             <h1 className="text-5xl leading-[57px] text-slate-900">
               Family Connect App
             </h1>
-            <h3 className="font-light mt-8 text-slate-500 leading-relaxed">This is our shared space to celebrate memories, explore our family tree, and stay connected. Sign in to view and share photos, find birthdays, and discover our roots. 
+            <h3 className="font-light mt-9 text-slate-500 leading-relaxed">This is our shared space to celebrate memories, explore our family tree, and stay connected. Sign in to view and share photos, find birthdays, and discover our roots. 
               <br/> Don't have an account yet? Create one and join the family online.</h3>
             <h3 className="font-light mt-8 text-slate-500">Aren't a family member? <a href="javascript:void(0);" className="text-blue-600 font-medium hover:underline ml-1">View a demo here.</a></h3>
           </div>
 
-          <div className="m-auto w-full mb-12">
+          <div className="w-full mb-12">
             <Authenticator
               signUpAttributes={[
                 'email',
